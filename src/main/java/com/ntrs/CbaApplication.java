c19320f690d2eae5dace9e9154162dd2366bf3e3
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2019-09-23 11:44:06 +0100

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/ntrs/CbaApplication.java
@@ -0,0 +1,12 @@
+package com.ntrs;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbaApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(CbaApplication.class, args);
+	}
+
+}
